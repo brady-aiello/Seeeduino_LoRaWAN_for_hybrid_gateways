cc0f6230847ccf0450223966d7378c9ab7de5c38
Brady <brady.aiello@gmail.com>
2017-11-07 16:07:18 -0800

    Removed old directory structure.
    Signed-off-by: Brady <brady.aiello@gmail.com>

--- Seeeduino/ABP_US_Test_Brady/keys_generic.h
+++
@@ -1,7 +0,0 @@
-#define DEV_ADDR  "Your Device Address"
-#define DEV_EUI   "Your Device EUI"
-#define APP_EUI   "Your App EUI"
-
-#define NWK_S_KEY "Your Network Session Key"
-#define APP_S_KEY "Your App Session Key"
-#define APP_KEY   "Your App Key"
